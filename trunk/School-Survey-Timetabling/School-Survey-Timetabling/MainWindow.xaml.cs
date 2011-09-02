2b72bfa7b22fd04043af366a3712c16e3e974517
julealgon@gmail.com <julealgon@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-09-02 21:02:34 +0000

    Animacao monstro.

--- trunk/School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
+++ trunk/School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Media;
 
 namespace School_Survey_Timetabling
 {
@@ -8,19 +9,29 @@ namespace School_Survey_Timetabling
     /// </summary>
     public partial class MainWindow
     {
+        public Transform Trans { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            // Insert code required on object creation below this point.
-            var ttt = teste.ToString();
+            Trans = new ScaleTransform(1.2, 1.2);
+            Resources.Add("teste", Trans);
 
             var teste = new Dictionary<string, Dictionary<string, Dictionary<string, List<Dictionary<List<int>, object>>>>>();
+            
+            // Insert code required on object creation below this point.
+            var ttt = teste.ToString();
 
             foreach (var t in teste)
             {
                    
             }
+        }
+
+        private void Button2_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
         }
     }
 }
