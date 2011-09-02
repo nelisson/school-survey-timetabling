fc4937118698010d7fc39faf903013087eb7238c
nelisson.ad@gmail.com <nelisson.ad@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-09-02 18:41:16 +0000

    teste de conflito

--- School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
+++ School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace School_Survey_Timetabling
 
             // Insert code required on object creation below this point.
             var teste = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
+            var ttt = teste.ToString();
 
             foreach (var t in teste)
             {
