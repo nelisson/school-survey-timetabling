c5a9f41361b8eba184191fa7e8b97d3172d6883d
julealgon@gmail.com <julealgon@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-09-03 22:12:10 +0000

    Cache do esquema. WTF conflito do nada q isso TT

--- School-Survey-Timetabling/School-Survey-Timetabling/Properties/AssemblyInfo.cs
+++ School-Survey-Timetabling/School-Survey-Timetabling/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Diagnostics.Contracts;
+
+[assembly: ContractVerification(false)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
