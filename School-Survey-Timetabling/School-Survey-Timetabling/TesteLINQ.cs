1ad6561152367eb212b02b4f5d12b5719bbdf35b
julealgon@gmail.com <julealgon@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-09-03 21:14:45 +0000

    Coisas de LINQ e testando os esquema de Contract (mto foda o/)

---
+++ School-Survey-Timetabling/School-Survey-Timetabling/TesteLINQ.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+
+namespace School_Survey_Timetabling
+{
+    [Table(Name = "TabelaTeste")]
+    public class TesteLINQ
+    {
+        [ContractVerification(false)]
+        public int ID { get; set; }
+        public float Teste { get; set; }
+
+        [ContractInvariantMethod]
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Required for code contracts.")]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(Teste >= 0);
+        }
+
+        public int M(string s)
+        {
+            Contract.Requires<ArgumentNullException>(s != null, "s nao pode ser null omg.");
+            //Contract.Ensures(Contract.Result<List<int>>().TrueForAll((i) => i > 0));
+            Contract.Ensures(Contract.Result<int>() > 0);
+            Teste = -3;
+            //return new List<int> { -1, 2, 3, 4, 5 };
+            return -5;
+        }
+
+        public void M2()
+        {
+            var x = M(null);
+        }
+    }
+}
