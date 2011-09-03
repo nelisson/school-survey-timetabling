4cccc3f5edc26a857e69dd314c9eaf807ec17554
nelisson.ad@gmail.com <nelisson.ad@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-09-03 03:15:47 +0000

    blá

--- School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
+++ School-Survey-Timetabling/School-Survey-Timetabling/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Media;
+﻿using System.Windows.Media;
 
 namespace School_Survey_Timetabling
 {
@@ -17,16 +15,6 @@ namespace School_Survey_Timetabling
 
             Trans = new ScaleTransform(1.2, 1.2);
             Resources.Add("teste", Trans);
-
-            var teste = new Dictionary<string, Dictionary<string, Dictionary<string, List<Dictionary<List<int>, object>>>>>();
-            
-            // Insert code required on object creation below this point.
-            var ttt = teste.ToString();
-
-            foreach (var t in teste)
-            {
-                   
-            }
         }
 
         private void Button2_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
