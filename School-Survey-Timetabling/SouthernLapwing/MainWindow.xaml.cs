db5ca5a8bab4610c33fd258aec4e5d6f10be1a3d
nelisson.ad@gmail.com <nelisson.ad@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-10-20 23:50:09 +0000

    Quero-Quero

---
+++ School-Survey-Timetabling/SouthernLapwing/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls.Ribbon;
+
+namespace SouthernLapwing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Insert code required on object creation below this point.
+        }
+
+        private void Schedule_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
