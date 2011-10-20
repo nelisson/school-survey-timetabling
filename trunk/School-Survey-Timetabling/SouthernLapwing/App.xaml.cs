722b91fdf02ec88b108e56207787ddd19cea14b4
nelisson.ad@gmail.com <nelisson.ad@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-10-20 23:50:09 +0000

    Quero-Quero

---
+++ trunk/School-Survey-Timetabling/SouthernLapwing/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace SouthernLapwing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
