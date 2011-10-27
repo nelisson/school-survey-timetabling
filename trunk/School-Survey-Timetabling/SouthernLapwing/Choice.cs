8d4ce4631fd04c2e3ee8ac9f85b8b1948e67b8b6
nelisson.ad@gmail.com <nelisson.ad@gmail.com@7cd1ba9b-6f3a-e354-930c-dfddd91ce3f4>
2011-10-27 11:12:20 +0000

    Adição da classe Choice para o Quero-quero, ajuste do clique

---
+++ trunk/School-Survey-Timetabling/SouthernLapwing/Choice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using School_Survey_Timetabling.Model;
+
+namespace SouthernLapwing
+{
+    class Choice
+    {
+        public int Priority { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public Shift Shift { get; set; }
+    }
+}
